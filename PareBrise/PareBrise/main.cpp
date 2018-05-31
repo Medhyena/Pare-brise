cd37ffec0210c4db5bbf9ef19207c4d947429208
Unknown <link684@msn.com>
2018-05-31 21:58:02 +0200

    Ajout de log+exp pour le pourcentage de luminosité/contraste
    Ajout d'images dans le fichiers pour tester + résultats des tests

--- PareBrise/PareBrise/main.cpp
+++ PareBrise/PareBrise/main.cpp
@@ -4,47 +4,10 @@
 
 #include<iostream>
 #include<string>
+#include<cmath>
 #include<conio.h>           // may have to modify this line if not using Windows
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-
-void Exemple()
-{
-
-	cv::Mat imgOriginal;        // input image
-	cv::Mat imgGrayscale;       // grayscale of input image
-	cv::Mat imgBlurred;         // intermediate blured image
-	cv::Mat imgCanny;           // Canny edge image
-
-	imgOriginal = cv::imread("image.png");          // open image
-
-	if (imgOriginal.empty()) {                                  // if unable to open image
-		std::cout << "error: image not read from file\n\n";     // show error message on command line
-		_getch();                                               // may have to modify this line if not using Windows
-		return;                                              // and exit program
-	}
-
-	cv::cvtColor(imgOriginal, imgGrayscale, CV_BGR2GRAY);       // convert to grayscale
-
-	cv::GaussianBlur(imgGrayscale,          // input image
-		imgBlurred,                         // output image
-		cv::Size(5, 5),                     // smoothing window width and height in pixels
-		1.5);                               // sigma value, determines how much the image will be blurred
-
-	cv::Canny(imgBlurred,           // input image
-		imgCanny,                   // output image
-		82,                         // low threshold
-		164);                       // high threshold
-
-									// declare windows
-	cv::namedWindow("imgOriginal", CV_WINDOW_AUTOSIZE);     // note: you can use CV_WINDOW_NORMAL which allows resizing the window
-	cv::namedWindow("imgCanny", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
-															// CV_WINDOW_AUTOSIZE is the default
-	cv::imshow("imgOriginal", imgOriginal);     // show windows
-	cv::imshow("imgCanny", imgCanny);
-
-	cv::waitKey(0);                 // hold windows open until user presses a key
-}
 
 void Diffentre2Img()
 {
@@ -78,6 +41,65 @@ void Diffentre2Img()
 	cv::imshow("imgDiffGray", imgDiffGray);
 
 	cv::imwrite("./test.png", imgDiffGray);
+
+	cv::waitKey(0);
+}
+
+void CannyTest()
+{
+	cv::Mat imgFog;
+	cv::Mat imgUnfog;
+	cv::Mat imgNous;
+
+	imgFog = cv::imread("imagefog6.png");
+
+	if (imgFog.empty()) {
+		std::cout << "error: image fog, incorrect name\n\n";
+		_getch();
+		return;
+	}
+
+	imgUnfog = cv::imread("imageunfog6.png");
+
+	if (imgUnfog.empty()) {
+		std::cout << "error: image unfog, incorrect name\n\n";
+		_getch();
+		return;
+	}
+
+	imgNous = cv::imread("imgDebrumeeNotreMethode63.png");
+
+	if (imgNous.empty()) {
+		std::cout << "error: image nous, incorrect name\n\n";
+		_getch();
+		return;
+	}
+
+	cv::cvtColor(imgFog, imgFog, CV_BGR2GRAY);
+	cv::cvtColor(imgUnfog, imgUnfog, CV_BGR2GRAY);
+	cv::cvtColor(imgNous, imgNous, CV_BGR2GRAY);
+
+	blur(imgFog, imgFog, cv::Size(3, 3));
+	blur(imgUnfog, imgUnfog, cv::Size(3, 3));
+	blur(imgNous, imgNous, cv::Size(3, 3));
+
+	cv::Canny(imgFog, imgFog, 20, 60, 3);
+	cv::Canny(imgUnfog, imgUnfog, 20, 60, 3);
+	cv::Canny(imgNous, imgNous, 20, 60, 3);
+
+	imgFog.convertTo(imgFog, CV_8U);
+	imgUnfog.convertTo(imgUnfog, CV_8U);
+	imgNous.convertTo(imgNous, CV_8U);
+
+	cv::imshow("image fog", imgFog);
+	cv::imshow("image unfog", imgUnfog);
+	cv::imshow("image nous", imgNous);
+
+	cv::imwrite("./imagecontourfog6.png", imgFog);
+	cv::imwrite("./imagecontourunfog6.png", imgUnfog);
+	cv::imwrite("./imagecontournous63.png", imgNous);
+
+	//cv::imwrite("./test.png", imgDiffGray);
 
 	cv::waitKey(0);
 }
@@ -170,19 +192,19 @@ void AlphaBlendingVideo(std::string nomVidBlend, std::string nomVidBackground, s
 
 void DiminuerLuminositePixel(cv::Vec3b &color, double pourcent)
 {
-	uchar contraste = (uchar) (pourcent * 255) / 2; // Meilleurs résultats quand on divise par deux
+	uchar luminosite = (uchar) (pourcent * 255) / 2; // Meilleurs résultats quand on divise par deux
 	// Red
-	int tmp = color[0] - contraste;
+	int tmp = color[0] - luminosite;
 	if (tmp > 255) color[0] = 255;
 	else if (tmp < 0) color[0] = 0;
 	else color[0] = (uchar) tmp;
 	// Green
-	tmp = color[1] - contraste;
+	tmp = color[1] - luminosite;
 	if (tmp > 255) color[1] = 255;
 	else if (tmp < 0) color[1] = 0;
 	else color[1] = (uchar) tmp;
 	// Blue
-	tmp = color[2] - contraste;
+	tmp = color[2] - luminosite;
 	if (tmp > 255) color[2] = 255;
 	else if (tmp < 0) color[2] = 0;
 	else color[2] = (uchar) tmp;
@@ -214,16 +236,19 @@ void DebrumageParContraste(std::string nomImgBrumee)
 
 	for (int i = 0; i < imgBrumee.rows; i++)
 	{
+		double pourcent = 1.0 - (double)i / (imgBrumee.rows - 1);
+		double pourcent2 = exp(((double)-i) / (imgBrumee.rows - 1));
+		double pourcent3 = log(exp(1) - (double)i / (((double)imgBrumee.rows * 3.0 / 4.0) - 1));
 		for (int j = 0; j < imgBrumee.cols; j++)
 		{
 			cv::Vec3b color = imgBrumee.at<cv::Vec3b>(cv::Point(j, i));
-			double pourcent = (double) 1 - (double) i / (imgBrumee.rows - 1);
-			DiminuerLuminositePixel(color, pourcent);
-			AugmenterContrastePixel(color, pourcent);
+			DiminuerLuminositePixel(color, pourcent3);
+			AugmenterContrastePixel(color, pourcent3);
 			imgBrumee.at<cv::Vec3b>(cv::Point(j, i)) = color;
 		}
 	}
 	cv::imshow("coucou", imgBrumee);
+	cv::imwrite("imgDebrumeeNotreMethode63.png", imgBrumee);
 	cv::Mat imgOrig = cv::imread(nomImgBrumee);
 	cv::Mat imgDiff;
 	cv::absdiff(imgBrumee, imgOrig, imgDiff);
@@ -235,6 +260,7 @@ void DebrumageParContrasteVideo(std::string nomVidBrumee)
 {
 	cv::VideoCapture capture(nomVidBrumee);
 	cv::Mat imgBrumee;
+	//cv::Mat imgCopy;
 
 	cv::namedWindow("w", 1);
 	for (; ; )
@@ -242,18 +268,21 @@ void DebrumageParContrasteVideo(std::string nomVidBrumee)
 		capture >> imgBrumee;
 		if (imgBrumee.empty())
 			break;
+		//imgCopy = imgBrumee.clone();
 		for (int i = 0; i < imgBrumee.rows; i++)
 		{
+			double pourcent = (double)1 - (double)i / (imgBrumee.rows - 1);
 			for (int j = 0; j < imgBrumee.cols; j++)
 			{
 				cv::Vec3b color = imgBrumee.at<cv::Vec3b>(cv::Point(j, i));
-				double pourcent = (double)1 - (double)i / (imgBrumee.rows - 1);
 				DiminuerLuminositePixel(color, pourcent);
 				AugmenterContrastePixel(color, pourcent);
 				imgBrumee.at<cv::Vec3b>(cv::Point(j, i)) = color;
 			}
 		}
 		cv::imshow("Rendu vidéo", imgBrumee);
+		//cv::imwrite("img720ptrainbrume.png", imgCopy);
+		//cv::imwrite("img720ptraindebrume.png", imgBrumee);
 		cv::waitKey(1); // waits to display frame
 	}
 	capture.release();
@@ -262,8 +291,9 @@ void DebrumageParContrasteVideo(std::string nomVidBrumee)
 int main()
 {
 	//AlphaBlendingImg("dashcam.jpg", "imageblend1.png", "alpha1.png");
-	//DebrumageParContraste("imagefog2.png");
-	DebrumageParContrasteVideo("test360p.mp4");
+	//DebrumageParContraste("imagefog6.png");
+	//DebrumageParContrasteVideo("test720p.mp4");
+	CannyTest();
 
 	return(0);
 }
